2eaef9adbde0f7658e2ec9d560d365c3dbb1a664
Mykola Bodnar <mykolab@nayax.com>
2022-11-25 14:12:48 +0200

    Init

---
+++ custom_components/ELKO_Telnet/entity.py
@@ -0,0 +1,53 @@
+"""ElkoEntity class"""
+from homeassistant.helpers import entity
+
+from .const import DOMAIN, VERSION, TITLE
+
+
+class ElkoEntity(entity.Entity):
+    def __init__(self, coordinator, device):
+        self.coordinator = coordinator
+        self.device = device
+
+    @property
+    def should_poll(self):
+        """No need to poll. Coordinator notifies entity of updates."""
+        return False
+
+    @property
+    def available(self):
+        """Return if entity is available."""
+        return self.coordinator.last_update_success
+
+    @property
+    def unique_id(self):
+        """Return a unique ID to use for this entity."""
+        return self.device.unique_id
+
+    @property
+    def device_info(self):
+        return {
+            "identifiers": {(DOMAIN, self.unique_id)},
+            "name": self.name,
+            "model": type(self).__name__,
+            "manufacturer": TITLE,
+            "sw_version": VERSION,
+        }
+
+    @property
+    def device_state_attributes(self):
+        """Return the state attributes."""
+        return {
+            "inels_type": type(self.device).__name__,
+            "inels_id": self.device.unique_id,
+        }
+
+    async def async_added_to_hass(self):
+        """Connect to dispatcher listening for entity data notifications."""
+        self.async_on_remove(
+            self.coordinator.async_add_listener(self.async_write_ha_state)
+        )
+
+    async def async_update(self):
+        """Update Brother entity."""
+        await self.coordinator.async_request_refresh()
