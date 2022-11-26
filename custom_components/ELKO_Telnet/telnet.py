19efaad8422f198795a77037dc8c7a44ce4efd97
Mykola Bodnar <mykolab@nayax.com>
2022-11-26 15:59:00 +0200

    Init

--- custom_components/ELKO_Telnet/telnet.py
+++ custom_components/ELKO_Telnet/telnet.py
@@ -5,11 +5,11 @@ import logging
 
 _LOGGER = logging.getLogger(__name__)
 
-def command(command, **kwargs ) -> str | None:
+def command(**kwargs ) -> str | None:
     try:
         _LOGGER.debug("Telnet connect to: %s with port: %s", kwargs['host'], kwargs['port'])
         telnet = telnetlib.Telnet(kwargs['host'], kwargs['port'])
-        telnet.write(command)
+        telnet.write(kwargs['telnet_command'])
         response = telnet.read_until(b"\r\n").decode('ascii').split(kwargs['delimiter'])[2].rstrip("\r").rstrip("\n")
         telnet.close()
     except OSError as error:
@@ -22,12 +22,16 @@ def command(command, **kwargs ) -> str | None:
 
 def getData( **kwargs ) -> str | None:
     _LOGGER.debug("kwargs: %s", kwargs.keys())
-    cmd = b"GET" + kwargs['delimiter'].encode('ascii') + kwargs['device_id'].encode('ascii') + b"\r\n"
-    _LOGGER.debug("command line: %s", cmd)
-    return command(cmd, kwargs)
+    kwargs.update({
+        'telnet_command': b"GET" + kwargs['delimiter'].encode('ascii') + kwargs['device_id'].encode('ascii') + b"\r\n",
+    })
+    _LOGGER.debug("command line: %s", kwargs['telnet_command'])
+    return command(**kwargs)
 
 def setData( **kwargs ) -> str | None:
     _LOGGER.debug("kwargs: %s", kwargs.keys())
-    cmd = b"SET" + kwargs['delimiter'].encode('ascii') + kwargs['device_id'].encode('ascii')+ kwargs['delimiter'].encode('ascii')+ kwargs['command'].encode('ascii') + b"\r\n"
-    _LOGGER.debug("command line: %s", cmd)
-    return command(cmd, kwargs)
+    kwargs.update({
+        'telnet_command': b"SET" + kwargs['delimiter'].encode('ascii') + kwargs['device_id'].encode('ascii')+ kwargs['delimiter'].encode('ascii')+ kwargs['command'].encode('ascii') + b"\r\n"
+    })
+    _LOGGER.debug("command line: %s", kwargs['telnet_command'])
+    return command(**kwargs)
